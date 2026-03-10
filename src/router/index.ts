5201ac17b31fccc5a79633c1195ad8011e1fa514
0jikuji0 <the.jikuji_dev@proton.me>
2026-03-10 10:55:27 +0100

    feat(auth): add login page with email, password and remember me fields no css

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { LoginPage } from '@/pages'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/auth/login',
+      component: LoginPage,
+    },
+  ],
 })
 
 export default router
