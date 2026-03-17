3b11d0d77140fa8fbf6acab624fb8c9696d35640
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-03-17 22:16:26 +0100

    feat(css): update page

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
-import api from '@/services/api.ts'
 import type { AuthCredentials } from '@/types/auth.ts'
 import { useAuthStore } from '@/stores/auth.ts'
 
@@ -13,50 +12,54 @@ const form = reactive<AuthCredentials>({
 })
 
 async function handleLogin() {
-  console.log('form:', form)
-
   await authStore.login(form)
 }
 </script>
 
 <template>
-  <div class="login-page">
-    <div class="login-card">
-      <h1>Connexion</h1>
+  <main class="layout-flex layout-columns layout-items-center layout-gap-large">
+    <hgroup>
+      <h1 class="semibold center">Connexion</h1>
+      <p class="small">Connecter vous à votre compte Upcycle Connect</p>
+    </hgroup>
 
-      <form @submit.prevent="handleLogin">
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input id="email" v-model="form.email" type="email" placeholder="vous@exemple.com" />
+    <form @submit.prevent="handleLogin" class="layout-small">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          class="primary medium full-width"
+          id="email"
+          v-model="form.email"
+          type="email"
+          placeholder="vous@exemple.com"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="password">Mot de passe</label>
+        <div class="form-group inline">
+          <input
+            class="primary medium full-width"
+            id="password"
+            v-model="form.password"
+            :type="showPassword ? 'text' : 'password'"
+            placeholder="••••••••"
+          />
+          <button class="primary medium" type="button" @click="showPassword = !showPassword">
+            {{ showPassword ? 'Masquer' : 'Afficher' }}
+          </button>
         </div>
+      </div>
 
-        <div class="form-group">
-          <div class="label-row">
-            <label for="password">Mot de passe</label>
-            <router-link to="forgot-password">Mot de passe oublié ?</router-link>
-          </div>
-          <div class="password-wrapper">
-            <input
-              id="password"
-              v-model="form.password"
-              :type="showPassword ? 'text' : 'password'"
-              placeholder="••••••••"
-            />
-            <button type="button" @click="showPassword = !showPassword">
-              {{ showPassword ? 'Masquer' : 'Afficher' }}
-            </button>
-          </div>
-        </div>
+      <div class="form-group">
+        <label>
+          <input type="checkbox" />
+          Se souvenir de moi
+        </label>
+      </div>
 
-        <div class="form-group">
-          <label>
-            <input type="checkbox" v-model="form.rememberMe" />
-            Se souvenir de moi
-          </label>
-        </div>
-
-        <button type="submit">Se connecter</button>
-      </form>
-    </div>
-  </div>
+      <button class="primary medium" type="submit">Se connecter</button>
+      <router-link class="ghost" to="forgot-password">Mot de passe oublié ?</router-link>
+    </form>
+  </main>
 </template>
