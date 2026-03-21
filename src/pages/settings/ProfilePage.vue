8db4269033d794bc505a02e0ca3108f540c84841
0jikuji0 <the.jikuji_dev@proton.me>
2026-03-21 20:26:13 +0100

    feat(css): Create header.css

--- src/pages/settings/ProfilePage.vue
+++ src/pages/settings/ProfilePage.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts"></script>
 <template>
-  <h1>Profile</h1>
+  <main class="layout-flex layout-columns layout-items-center layout-gap-large">
+    <hgroup>
+      <h1 class="semibold center">Profile</h1>
+    </hgroup>
+    <div class="layout-small">
+      <div class="form-group">
+        <label for="name">Nom</label>
+      </div>
+    </div>
+  </main>
 </template>
