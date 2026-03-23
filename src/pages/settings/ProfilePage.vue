234dcd5215d494d0ffee78d8d54970261604ca4e
0jikuji0 <the.jikuji_dev@proton.me>
2026-03-23 19:44:47 +0100

    feat(css): test for class header layout-full-width container

--- src/pages/settings/ProfilePage.vue
+++ src/pages/settings/ProfilePage.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts"></script>
 <template>
-  <main class="layout-flex layout-columns layout-items-center layout-gap-large">
-    <hgroup>
-      <h1 class="semibold center">Profile</h1>
-    </hgroup>
-    <div class="layout-small">
+  <main class="">
+    <header class="header layout-full-width container">fz</header>
+    <div class="layout-full-height">
+      <hgroup>
+        <h1 class="semibold center">Profile</h1>
+      </hgroup>
       <div class="form-group">
         <label for="name">Nom</label>
       </div>
